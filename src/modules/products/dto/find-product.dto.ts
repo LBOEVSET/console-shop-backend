6b343a1ed8cb93fd9a56eb5f9d188adc5d41a871
Sorawich <sorawich.t@entronica.in.th>
2026-03-06 10:49:57 +0700

    change_product_list_dto_to_optional

--- src/modules/products/dto/find-product.dto.ts
+++ src/modules/products/dto/find-product.dto.ts
@@ -5,24 +5,31 @@ import {
   Min,
   IsUUID,
   IsBoolean,
+  IsOptional,
 } from 'class-validator';
 
 export class FindProductDto {
+  @IsOptional()
   @IsString()
   searchWord?: string;
 
+  @IsOptional()
   @IsBoolean()
   inStock?: boolean;
 
+  @IsOptional()
   @IsNumber()
   minPrice?: number;
 
+  @IsOptional()
   @IsNumber()
   maxPrice?: number;
 
+  @IsOptional()
   @IsUUID()
   platformId?: string;
 
+  @IsOptional()
   @IsUUID()
   categoryId?: string;
 }
