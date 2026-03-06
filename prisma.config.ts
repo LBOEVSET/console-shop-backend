1bd4139ee72777fcc48ec3198016a6f098c89690
Sorawich <sorawich.t@entronica.in.th>
2026-03-06 10:12:02 +0700

    correct_prisma_config_path

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import { join } from "path";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: join(process.cwd(), "prisma/schema.prisma"),
 
   migrations: {
     seed: "ts-node prisma/seed.ts",
