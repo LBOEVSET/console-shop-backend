06d568c6420f759465f652f9a410a4fef2b4f40c
Sorawich <sorawich.t@entronica.in.th>
2026-03-08 01:01:28 +0700

    make_health_check_public

--- src/modules/health-check/heclth-check.controller.ts
+++ src/modules/health-check/heclth-check.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/common/decorators/public.decorator';
 
+@Public()
 @Controller('health')
 export class HealthController {
   @Get()
